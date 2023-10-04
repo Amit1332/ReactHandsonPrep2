d9ee600700e9e192fa5212d92859e94eb08e9cb8
Amit1332 <amitsinghpatel9747@gmail.om>
2023-10-04 13:17:31 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { FormClass } from './Components/FormValidation/FormClass';
+import { Hello } from './Components/Hello';
+
+
+function App() {
+  return (
+   <div className='App'>
+    <Hello/>
+    <FormClass/>
+   </div>
+  );
+}
+
+export default App;
